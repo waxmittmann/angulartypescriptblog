1b6a9356a09ed2b4eb5e346ccc5a973632b73f4f
Maximilian Wittmann <maximilian.wittmann@covata.com>
2015-10-28 12:43:32 +1100

    Basic routing working, yay

--- js/Application.js
+++ js/Application.js
@@ -46,13 +46,64 @@ var blogposts;
     })();
     blogposts.BlogPostCtrl = BlogPostCtrl;
 })(blogposts || (blogposts = {}));
+/// <reference path="libs/angular/angular.d.ts" />
+/// <reference path="libs/angular/angular-route.d.ts" />
+'use strict';
+// Create and register modules
+// var modules = ['app.controllers','app.directives', 'app.filters', 'app.services'];
+// modules.forEach((module) => angular.module(module, []));
+// *** Push ngRoute or $routeProvider won't work ***
+// modules.push("ngRoute");
+// angular.module('app', modules);
+// Url routing
+// angular.module('golby').config(['$routeProvider',
+//     function routes($routeProvider: ng.route.IRouteProvider) { // *** $routeProvider is typed with ng.route.IRouteProvider ***
+//         $routeProvider
+//             .when('/', {
+//                 templateUrl: 'views/MyView.html',
+//                 controller: 'golby.controllers.BlogPostCtrl'
+//             })
+//             .when('/cheese', {
+//                 templateUrl: 'views/Cheese.html',
+//                 controller: 'golby.controllers.BlogPostCtrl'
+//             })
+//             .otherwise({
+//                 redirectTo: '/'
+//             });
+//     }
+// ]);
 /// <reference path='libs/jquery/jquery.d.ts' />
 /// <reference path='libs/angular/angular.d.ts' />
 /// <reference path='BlogPostCtrl.ts' />
+/// <reference path='Router.ts' />
+/// <reference path="libs/angular/angular-route.d.ts" />
 var blogposts;
 (function (blogposts) {
     'use strict';
-    var golby = angular.module('golby', [])
-        .controller('blogPostCtrl', blogposts.BlogPostCtrl);
+    // var modules = ['app.controllers','app.directives', 'app.filters', 'app.services'];
+    // modules.forEach((module) => angular.module(module, []));
+    // *** Push ngRoute or $routeProvider won't work ***
+    // modules.push("ngRoute");
+    // angular.module('app', modules);
+    var golby = angular.module('golby', ['ngRoute'])
+        .controller('blogPostCtrl', blogposts.BlogPostCtrl)
+        .config(['$routeProvider',
+        // *** $routeProvider is typed with ng.route.IRouteProvider ***
+        // *** $routeProvider is typed with ng.route.IRouteProvider ***
+        function routes($routeProvider) {
+            $routeProvider
+                .when('/', {
+                templateUrl: 'views/view1.html',
+                controller: 'blogPostCtrl'
+            })
+                .when('/cheese', {
+                templateUrl: 'views/view2.html',
+                controller: 'blogPostCtrl'
+            })
+                .otherwise({
+                redirectTo: '/'
+            });
+        }
+    ]);
 })(blogposts || (blogposts = {}));
 //# sourceMappingURL=Application.js.map
--- ts/Application.ts
+++ ts/Application.ts
@@ -1,10 +1,38 @@
 /// <reference path='libs/jquery/jquery.d.ts' />
 /// <reference path='libs/angular/angular.d.ts' />
 /// <reference path='BlogPostCtrl.ts' />
+/// <reference path='Router.ts' />
+/// <reference path="libs/angular/angular-route.d.ts" />
 
 module blogposts {
     'use strict';
 
-    var golby = angular.module('golby', [])
-            .controller('blogPostCtrl', BlogPostCtrl);
+    // var modules = ['app.controllers','app.directives', 'app.filters', 'app.services'];
+    // modules.forEach((module) => angular.module(module, []));
+
+    // *** Push ngRoute or $routeProvider won't work ***
+    // modules.push("ngRoute");
+
+    // angular.module('app', modules);
+
+    var golby = angular.module('golby', ['ngRoute'])
+    // var golby = angular.module('golby', ['angular-route'])
+            .controller('blogPostCtrl', BlogPostCtrl)
+            .config(['$routeProvider',
+                // *** $routeProvider is typed with ng.route.IRouteProvider ***
+                function routes($routeProvider: ng.route.IRouteProvider) {
+                    $routeProvider
+                        .when('/', {
+                            templateUrl: 'views/view1.html',
+                            controller: 'blogPostCtrl'
+                        })
+                        .when('/cheese', {
+                            templateUrl: 'views/view2.html',
+                            controller: 'blogPostCtrl'
+                        })
+                        .otherwise({
+                            redirectTo: '/'
+                        });
+                }
+            ]);
 }
